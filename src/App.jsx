ad848dba692b3931176c59d51f0b875d126fe42a
mohammadafzalsiddiqui <mohammadafzalsiddiqui41@gmail.com>
2024-12-04 14:32:56 +0530

    new commits

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import { Route, Routes } from "react-router-dom";
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+const App =()=>{
+  return( <div>
+   <Routes>
+    <Route path="/" element={<h1>Home Page</h1>}></Route>
+    <Route path="/counter" element={<h1>Counter App</h1>}></Route>
+    <Route path="/stopwatch" element={<h1>Stop watch App</h1>}></Route>
+    <Route path="*" element={<h1>No page available</h1>}></Route>
+    </Routes> 
+  </div>
+  )
+}
+export default App;
